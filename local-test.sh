dcb1b5cb934976b699c6b3091fc19cddc0a228d2
Alexander Berger <alex.berger@nexxiot.com>
2025-01-08 13:24:41 +0000

    feat: upgrade to k8s 1.32

--- local-test.sh
+++ local-test.sh
@@ -3,7 +3,7 @@
 export KIND=3e82e061-dae1-4b88-a760-549d3f9c161d
 export KUBECONFIG=".kube-config"
 
-kind create cluster --name $KIND --kubeconfig $KUBECONFIG --image 'kindest/node:v1.29.2'
+kind create cluster --name $KIND --kubeconfig $KUBECONFIG --image 'kindest/node:v1.32.0'
 
 kubectl delete deployment -n k8s-gitops-secrets-system k8s-gitops-secrets-controller-manager
 
